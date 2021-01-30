05cf3935ca9bc19975b1e07861e990967b8a7b0e
Caleb Stouter <cdstouter@gmail.com>
2021-01-29 22:26:12 -0500

    removed tensorflow requirement

--- keras_unet/metrics.py
+++ keras_unet/metrics.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+#import tensorflow as tf
 
 from keras_unet import TF
 if TF:
@@ -21,18 +21,18 @@ def jaccard_coef(y_true, y_pred):
     return K.mean(jac)
 
 
-def threshold_binarize(x, threshold=0.5):
-    ge = tf.greater_equal(x, tf.constant(threshold))
-    y = tf.where(ge, x=tf.ones_like(x), y=tf.zeros_like(x))
-    return y
+# def threshold_binarize(x, threshold=0.5):
+#     ge = tf.greater_equal(x, tf.constant(threshold))
+#     y = tf.where(ge, x=tf.ones_like(x), y=tf.zeros_like(x))
+#     return y
 
 
-def iou_thresholded(y_true, y_pred, threshold=0.5, smooth=1.):
-    y_pred = threshold_binarize(y_pred, threshold)
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
+# def iou_thresholded(y_true, y_pred, threshold=0.5, smooth=1.):
+#     y_pred = threshold_binarize(y_pred, threshold)
+#     y_true_f = K.flatten(y_true)
+#     y_pred_f = K.flatten(y_pred)
+#     intersection = K.sum(y_true_f * y_pred_f)
+#     return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
 
 
 def dice_coef(y_true, y_pred, smooth=1.):
